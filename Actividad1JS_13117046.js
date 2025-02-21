f65a29cd82747f2f193eb4f3d96cee3c98e4ca2a
Alvaro Chilel <13117046@galileo.edu>
2025-02-20 23:27:56 -0600

    primer commit

---
+++ Actividad1JS_13117046.js
@@ -0,0 +1,99 @@
+var mes = prompt("Ingrese el mes")
+var dia = prompt("Ingrese el día")
+
+   if(mes == 1){
+     if(dia <= 20){
+       alert("Su signo zodiacal es Capricornio")
+     }if(dia >= 21){
+       alert("Su signo zodiacal es Acuario")
+     }
+   }
+
+   if(mes == 2){
+     if(dia <= 19){
+       alert("Su signo zodiacal es Acuario")
+     }if(dia >= 20){
+       alert("Su signo zodiacal es Picis")
+     }
+   }
+
+if(mes == 3){
+     if(dia <= 20){
+       alert("Su signo zodiacal es Picis")
+     }if(dia >= 21){
+       alert("Su signo zodiacal es Aries")
+     }
+   }
+
+if(mes == 4){
+     if(dia <= 20){
+       alert("Su signo zodiacal es Aries")
+     }if(dia >= 21){
+       alert("Su signo zodiacal es Tauro")
+     }
+   }
+
+if(mes == 5){
+     if(dia <= 20){
+       alert("Su signo zodiacal es Tauro")
+     }if(dia >= 21){
+       alert("Su signo zodiacal es Geminis")
+     }
+   }
+
+if(mes == 6){
+     if(dia <= 20){
+       alert("Su signo zodiacal es Geminis")
+     }if(dia >= 21){
+       alert("Su signo zodiacal es Cancer")
+     }
+   }
+
+if(mes == 7){
+     if(dia <= 22){
+       alert("Su signo zodiacal es Cancer")
+     }if(dia >= 23){
+       alert("Su signo zodiacal es Leo")
+     }
+   }
+
+if(mes == 8){
+     if(dia <= 23){
+       alert("Su signo zodiacal es Leo")
+     }if(dia >= 24){
+       alert("Su signo zodiacal es Virgo")
+     }
+   }
+
+if(mes == 9){
+     if(dia <= 22){
+       alert("Su signo zodiacal es Virgo")
+     }if(dia >= 23){
+       alert("Su signo zodiacal es Libra")
+     }
+   }
+
+if(mes == 10){
+     if(dia <= 23){
+       alert("Su signo zodiacal es Libra")
+     }if(dia >= 24){
+       alert("Su signo zodiacal es Escorpio")
+     }
+   }
+
+if(mes == 11){
+     if(dia <= 22){
+       alert("Su signo zodiacal es Escorpio")
+     }if(dia >= 23){
+       alert("Su signo zodiacal es Sagitario")
+     }
+   }
+
+if(mes == 12){
+     if(dia <= 21){
+       alert("Su signo zodiacal es Sagitario")
+     }if(dia >= 22){
+       alert("Su signo zodiacal es Capricornio")
+     }
+   }
+
